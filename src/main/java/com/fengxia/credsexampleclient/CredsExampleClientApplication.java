0ffef5e53259e57536155132455e8beccb874927
Feng Xia <feng.xia@gmail.com>
2021-01-31 12:13:31 -0500

    Initial Submit again

---
+++ src/main/java/com/fengxia/credsexampleclient/CredsExampleClientApplication.java
@@ -0,0 +1,13 @@
+package com.fengxia.credsexampleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredsExampleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredsExampleClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fengxia/credsexampleclient/CredsExampleClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengxia.credsexampleclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredsExampleClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
